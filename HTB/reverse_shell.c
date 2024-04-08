4adde057879aa181686fdbedfb5e2ad55bbbd4fd
Harshal Dulera <harshaldulera82@gmail.com>
2024-04-08 08:40:02 +0530

    new

--- HTB/reverse_shell.c
+++ HTB/reverse_shell.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include <sys/stockets.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
